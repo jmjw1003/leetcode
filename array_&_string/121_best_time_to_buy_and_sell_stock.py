2fd7e44883578bcbe9a3ed36b7a8d758386509db
James Watson <jmjw1991@outlook.com>
2025-01-05 16:20:29 +0000

    Create 121_best_time_to_buy_and_sell_stock.py

---
+++ array_&_string/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+        while r < len(prices):
+            profit = prices[r] - prices[l]
+            if profit < 0:
+                l = r
+                r += 1
+            else:
+                max_profit = max(max_profit, profit)
+                r += 1
+        return max_profit
