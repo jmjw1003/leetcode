0fd7357cb665e62ab0d0de6cfc8de522b5cda8f1
James Watson <jmjw1991@outlook.com>
2025-02-03 23:22:59 +0000

    Update 55_jump_game.py
    Solved it again, added explanations

--- array_&_string/55_jump_game.py
+++ array_&_string/55_jump_game.py
@@ -1,9 +1,14 @@
 class Solution:
     def canJump(self, nums: list[int]) -> bool:
-        goal = len(nums) - 1
+        target = len(nums) - 1 # Current goal: reach last index
 
-        for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-        
-        return goal == 0
+        # Iterate backwards through the array starting at second last index
+        for idx in range(target - 1, -1, -1):
+            # Calculate how far we can move from this index
+            max_distance = idx + nums[idx]
+            # If we can reach the goal from here, this becomes the new target
+            if max_distance >= target:
+                target = idx
+
+        # If the target has reached the first element in the array we can reach the last index
+        return target == 0
