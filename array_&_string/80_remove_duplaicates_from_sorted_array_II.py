095521570b783f42857e3b57078b18081f315db7
James Watson <jmjw1991@outlook.com>
2025-02-03 22:03:56 +0000

    Update 80_remove_duplaicates_from_sorted_array_II.py

--- array_&_string/80_remove_duplaicates_from_sorted_array_II.py
+++ array_&_string/80_remove_duplaicates_from_sorted_array_II.py
@@ -1,5 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
+        # Did it again, prefer this solution
+        removals = 0
+        for i in range(2, len(nums)):
+            idx = i - removals
+            if nums[idx] == nums[idx - 2]:
+                nums.pop(idx)
+                removals += 1
+
+    def removeDuplicates(self, nums: list[int]) -> int:
         last_seen = [nums[0], 1]
         removed = 0
         for i in range(1, len(nums)):
