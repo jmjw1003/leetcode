c56f7c570cb87a22e419c5f758529c99d5fbcbda
James Watson <jmjw1991@outlook.com>
2025-02-20 21:56:18 +0000

    Update 735_asteroid_collision.py

--- stack_/735_asteroid_collision.py
+++ stack_/735_asteroid_collision.py
@@ -1,27 +1,25 @@
 class Solution:
     def asteroidCollision(self, asteroids: list[int]) -> list[int]:
         stack = []
+        
         for asteroid in asteroids:
-            if asteroid > 0:
+            if not stack:
                 stack.append(asteroid)
+            # R + R, or L + any direction
+            elif (stack[-1] > 0 and asteroid > 0) or (stack[-1] < 0):
+                stack.append(asteroid)
+            # R + L -> clash
             else:
-                right = abs(asteroid)
-                if not stack or stack[-1] < 0:
-                    stack.append(asteroid)
-                else:
-                    while stack and right and not stack[-1] < 0:
-                        if stack[-1] == right:
-                            stack.pop()
-                            right = None
-                        elif stack[-1] > right:
-                            right = None
-                        else:
-                            stack.pop()
-                    if right:
-                        stack.append(asteroid)
+                # Round 1, FIGHT!
+                while stack and stack[-1] > 0:
+                    if stack[-1] == abs(asteroid):
+                        stack.pop()
+                        break
+                    elif stack[-1] < abs(asteroid):
+                        stack.pop()
+                        if not stack or stack[-1] < 0:
+                            stack.append(asteroid)
+                            break
+                    else:
+                        break
         return stack
-    
-
-# x = Solution().asteroidCollision([5,-2,-1,1,2])
-x = Solution().asteroidCollision([-2,-2,1,-2])
-print(x)
