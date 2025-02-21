efb141014d9fbcb6fe1598ab735bd24f48e838aa
James Watson <jmjw1991@outlook.com>
2025-02-21 22:00:38 +0000

    Update 173_binary_search_tree_iterator.py

--- stack_/173_binary_search_tree_iterator.py
+++ stack_/173_binary_search_tree_iterator.py
@@ -14,14 +14,37 @@ class BSTIterator:
     def __init__(self, root: Optional[TreeNode]):
         self.stack = []
         self._populate_stack(root)
-        
+
     def next(self) -> int:
         next_node = self.stack.pop()
         self._populate_stack(next_node.right)
         return next_node.val
-        
+
     def hasNext(self) -> bool:
         return True if self.stack else False
+
+
+class BSTIterator2:
+    """Pre-processed tree"""
+    def _traverse_tree(self, node):
+        if node.left:
+            self._traverse_tree(node.left)
+        self.node_values.append(node.val)
+        if node.right:
+            self._traverse_tree(node.right)
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.node_values = []
+        if root:
+            self._traverse_tree(root)
+        self.ptr = 0
+
+    def next(self) -> int:
+        self.ptr += 1
+        return self.node_values[self.ptr - 1]
+
+    def hasNext(self) -> bool:
+        return self.ptr < len(self.node_values)
 
 
 # Your BSTIterator object will be instantiated and called as such:
