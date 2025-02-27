cbee409e2d5d1f831bc24de1dd9437a838ba0ac5
James Watson <jmjw1991@outlook.com>
2025-02-27 16:33:14 +0000

    Update 198_house_robber.py

--- dynamic_programming/198_house_robber.py
+++ dynamic_programming/198_house_robber.py
@@ -1,8 +1,21 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    def rob(self, nums: list[int]) -> int:
         rob_left = rob_right = 0
         # Always keep track of max from pervious two houses
         # [rob_left, rob_right, num, num + 1, ...]
         for num in nums:
             rob_left, rob_right = rob_right, max(rob_left + num, rob_right)
         return rob_right
+
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        houses = len(nums)
+        if houses < 3:
+            return max(nums)
+        
+        totals = [nums[-2], nums[-1]]
+
+        for house in range(houses - 3, -1, -1):
+            totals[0], totals[1] = max(nums[house] + totals[1], totals[0]), max(totals[0], totals[1])
+    
+        return totals[0]
