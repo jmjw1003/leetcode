15a50ce5959f8ddfbd285a39c65b5cabdc9946fd
James Watson <jmjw1991@outlook.com>
2025-03-08 21:22:16 +0000

    Create 509_fibonacci_number.py

---
+++ dynamic_programming/509_fibonacci_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+
+        dp = [0, 1]
+        for i in range(2, n + 1):
+            dp[0], dp[1] = dp[1], dp[0] + dp[1]
+
+        return dp[1]
